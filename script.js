6393a7ace7df6c1bd8c28873e55741868947d70a
Azamat <Azamat>
2021-09-22 23:50:43 +0600

    init

---
+++ script.js
@@ -0,0 +1,104 @@
+//Begin1
+
+// let a = 3
+// let P = 4 * a
+// console.log(`Периметр:${P}`)
+
+//Begin2
+
+// let a = 8
+// let S = a * a 
+
+// console.log(`Площадь:${S}`)
+
+//Begin3
+// let a = 8
+// let b = 5
+// let S  = a * b
+// let P = 2*(a+b)
+// console.log(`Площадь:${S}, Периметр:${P}`)
+
+//Begin4
+// let d = 7
+// let L = 3.14 * d
+// console.log(`Длина:${L}`)
+
+//Begin5
+// let a = 4
+// let V = a * a * a
+// let S = 6 * a * a
+// console.log(`Объем:${V} Площадь:${S}`)
+
+//Begin6
+// let a = 7
+// let b = 4
+// let c = 4
+// let V = a * b * c
+// let S = 2*(a*b + b*c + a*c)
+// console.log(`Длина: ${V} Объем: ${S}`)
+
+//Begin7
+// let R = 4
+// let L = 2*3.14*R
+// let S = 3.14*R * R
+// console.log(`Длина:${L} Площадь: ${S}`);
+
+//Begin8
+// let a = 7
+// let b = 5
+// let r = (a+b)/2
+// console.log(`Середина:${r}`);
+
+//Begin9
+//  let a = 5
+//  let b = 5
+//  let k = (a * b)
+//  console.log(`Середина: ${k}`);
+
+//Begin10
+// let a = 4
+// let b = 4
+// let c = a*a + b*b
+// let d = a*a - b*b
+// let e = a*a * b*b
+// let m = a*a / b*b
+// console.log(`Сумма: ${c} Разность ${d} Произведение: ${e} Частное: ${m}`)
+
+//Begin11
+// let a = 4
+// let b = 4
+// let c = (a) + (b)
+// let d = a - b
+// let e = a * b
+// let m = a / b
+// console.log(`Сумма: ${c} Разность ${d} Произведение: ${e} Частное: ${m}`)
+
+//Begin12
+// let a = 4
+// let b = 5
+// let c = a*a + b*b
+// let P = a+b+c
+// console.log(`Гипотеза:${c} Приметр:${P}`);
+
+//Begin13
+// let R1 = 5
+// let R2 = 5
+// let S1 = 3.14*R1*R1
+// let S2 = 3.14*R2*R2
+// let S3 = S1-S2
+// console.log(`Площадь1: ${S1} Площадь2: ${S2} Площадь3: ${S3}`);
+
+
+//Begin14
+// let L = 7
+// let R = L / 3.14*2
+// let S = 3.14*R*R
+// console.log(`Радиус:${R.toFixed()} Площадь:${S.toFixed()}`);
+
+
+//Begin15
+
+// let S = 8
+// let D = 3.14*4/S
+// let L = 3.14*D
+// console.log(`Диаметр:${D} Длина:${L}`);
